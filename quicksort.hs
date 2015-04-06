60b2119c0adcef24676082612aeda631eb92f5f5
Frank Hucek <fjh32@case.edu>
2015-04-06 18:03:36 -0400

    init

---
+++ quicksort.hs
@@ -0,0 +1,33 @@
+import System.Random 
+import Data.List  
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = quicksort smallerequal ++ [x] ++ quicksort greater
+    where smallerequal = [e | e <- xs, e <= x]
+          greater = [e | e <- xs, e > x]
+
+dpquicksort :: (Ord a) => [a] -> [a]
+dpquicksort [] = []
+dpquicksort (x:y) = quicksort (x:y)
+dpquicksort (x:y:xs) = 
+    dpquicksort smaller ++ [a] ++ dpquicksort xgteqylteq ++ [b] ++ dpquicksort greater
+    where a = min x y
+          b = max x y
+          smaller = [e | e <- xs, e < x]
+          xgteqylteq = [e | e <- xs, e >= x, e <= y]
+          greater = [e | e <- xs, e > y]
+
+randomlist :: Int -> StdGen -> [Int]
+randomlist n = take n . unfoldr (Just . random) 
+
+main = do
+    seed <- newStdGen 
+    
+    let list = randomlist 1000000 seed
+    let something = map (`mod` 1000000) list
+    let sorted = dpquicksort something
+    print(sorted)
+   
+    
+
