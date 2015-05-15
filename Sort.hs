fa7114acde086419ff10d24e9b538b7a14baac95
Miles Frankel <miles.frankel@gmail.com>
2015-05-15 10:46:38 -0400

    efficient random list generatio

--- Sort.hs
+++ Sort.hs
@@ -3,21 +3,23 @@ import qualified Data.Vector.Unboxed as U
 import System.Random
 import System.CPUTime -- timer
 
-qsort :: U.Vector Int -> U.Vector Int
+qsort :: (U.Unbox a, Ord a) => U.Vector a -> U.Vector a
 qsort = U.modify go
   where go xs | M.length xs < 2 = return ()
               | otherwise = do
-                  n <- M.read xs (M.length xs `div` 2)         -- get value of number in middle
-                  p <- M.unstablePartition (< n) xs            -- split list in place about n, return split index
-                  let (l, rWithDups) = M.splitAt p xs          -- conceptually split list
-                  dups <- M.unstablePartition (== n) rWithDups -- drop duplicates from right list
-                  go l                                         -- recurse
-                  go $ M.drop dups rWithDups
+                  n <- M.read xs (M.length xs `div` 2)
+                  p <- M.unstablePartition (< n) xs
+                       -- split list in place about n, return split index
+                  let (l, rWithDups) = M.splitAt p xs
+                       -- conceptually split list
+                  ns <- M.unstablePartition (== n) rWithDups
+                       -- drop copies of n from right list
+                  go l -- recurse
+                  go $ M.drop ns rWithDups
 
 main :: IO ()
-main = do l <- mapM (const $ getStdRandom(randomR (1,1000000))) [1..1000000 :: Int]
+main = do v  <- U.replicateM 1000000 (getStdRandom(randomR (1,1000000 :: Int)))
           s <- getCPUTime
-          let v = U.fromList l
-          print $ qsort v `seq` "done"
+          print $ qsort v  `seq` "done"
           e <- getCPUTime
           putStrLn $ show (fromIntegral (e - s) / 1000000000000) ++ " seconds"
