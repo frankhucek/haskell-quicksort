9a857ff0147c07029d8a1927bb6d5b3d9b76c3c0
Miles Frankel <miles.frankel@gmail.com>
2015-05-15 00:21:34 -0400

    formatting

--- Sort.hs
+++ Sort.hs
@@ -7,15 +7,15 @@ qsort :: U.Vector Int -> U.Vector Int
 qsort = U.modify go
   where go xs | M.length xs < 2 = return ()
               | otherwise = do
-                  n <- M.read xs (M.length xs `div` 2)   -- get value of number in middle
-                  p <- M.unstablePartition (< n) xs      -- split list in place about n, return split index
-                  let (l, rWithDups) = M.splitAt p xs           -- conceptually split list
-                  dups <- M.unstablePartition (== n) rWithDups     -- drop duplicates from right list
-                  go l                                   -- recurse
+                  n <- M.read xs (M.length xs `div` 2)         -- get value of number in middle
+                  p <- M.unstablePartition (< n) xs            -- split list in place about n, return split index
+                  let (l, rWithDups) = M.splitAt p xs          -- conceptually split list
+                  dups <- M.unstablePartition (== n) rWithDups -- drop duplicates from right list
+                  go l                                         -- recurse
                   go $ M.drop dups rWithDups
 
 main :: IO ()
-main = do l <- mapM (const $ getStdRandom(randomR (1,1000000))) [1..1000000 :: Int] :: IO [Int]
+main = do l <- mapM (const $ getStdRandom(randomR (1,1000000))) [1..1000000 :: Int]
           s <- getCPUTime
           let v = U.fromList l
           print $ qsort v `seq` "done"
